92a0ec26c19443cb2c89a7f45a3063e969369a67
Jaden Balogh <jadenbalogh@gmail.com>
2021-01-02 18:39:47 -0800

    Update planet demo scene

---
+++ Assets/Scripts/Moon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : Planet
+{
+    public Planet orbitPlanet;
+    public Vector3 orbitAxis;
+    public float orbitSpeed;
+
+    new void Update()
+    {
+        transform.RotateAround(orbitPlanet.transform.position, orbitAxis, orbitSpeed * Time.deltaTime);
+
+        base.Update();
+    }
+}
---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public Vector3 rotationAxis;
+    public float rotationSpeed;
+
+    protected void Update()
+    {
+        transform.RotateAround(transform.position, rotationAxis, rotationSpeed * Time.deltaTime);
+    }
+}
