79e0d3a40b9b797d41adf8fb02589bb331c3a42e
Jaden Balogh <jadenbalogh@gmail.com>
2021-01-04 08:24:34 -0800

    Fix bugs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void PlayGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex +1);
     }
+
     public void QuitGame()
     {
         Application.Quit();
