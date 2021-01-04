ffad9366734c3fcca1c46beb41d38729f4d38734
Jaden Balogh <jadenbalogh@gmail.com>
2021-01-04 00:12:43 -0800

    Update LAIKA HUD

--- Assets/Scripts/LaikaHUD.cs
+++ Assets/Scripts/LaikaHUD.cs
@@ -7,19 +7,28 @@ public class LaikaHUD : MonoBehaviour
     public static LaikaHUD instance;
 
     public TMPro.TextMeshProUGUI textbox;
+    public Light holoLight; 
+
     public LaikaMessage[] messages;
+    public float enableDelay = 0.3f;
     public float charDelay = 0.05f;
     public float wordDelay = 0.07f;
     public float clearDelay = 10f;
     public float deleteDelay = 0.05f;
+    public float disableDelay = 0.5f;
 
+    private int messageIndex = 0;
+    private bool isBusy = false;
+    private bool hudEnabled = false;
+
+    private WaitForSeconds enableDelayInstruction;
     private WaitForSeconds charDelayInstruction;
     private WaitForSeconds wordDelayInstruction;
     private WaitForSeconds clearDelayInstruction;
     private WaitForSeconds deleteDelayInstruction;
+    private WaitForSeconds disableDelayInstruction;
 
-    private int messageIndex = 0;
-    private bool isBusy = false;
+    private CanvasGroup hudGroup;
 
     void Awake()
     {
@@ -30,10 +39,14 @@ public class LaikaHUD : MonoBehaviour
         }
         instance = this;
 
+        enableDelayInstruction = new WaitForSeconds(enableDelay);
         charDelayInstruction = new WaitForSeconds(charDelay);
         wordDelayInstruction = new WaitForSeconds(wordDelay);
         clearDelayInstruction = new WaitForSeconds(clearDelay);
         deleteDelayInstruction = new WaitForSeconds(deleteDelay);
+        disableDelayInstruction = new WaitForSeconds(disableDelay);
+
+        hudGroup = GetComponent<CanvasGroup>();
     }
 
     void Update()
@@ -42,6 +55,20 @@ public class LaikaHUD : MonoBehaviour
         {
             WriteRandomMessage();
         }
+    }
+
+    public void EnableHUD()
+    {
+        holoLight.enabled = true;
+        EnableHUDGroup(true);
+        hudEnabled = true;
+    }
+
+    public void DisableHUD()
+    {
+        holoLight.enabled = false;
+        EnableHUDGroup(false);
+        hudEnabled = false;
     }
 
     public bool IsLaikaBusy()
@@ -72,7 +99,13 @@ public class LaikaHUD : MonoBehaviour
     private IEnumerator WriteTimedMessage(string message)
     {
         isBusy = true;
-        textbox.text = "> ";
+
+        if (!hudEnabled)
+        {
+            EnableHUD();
+            textbox.text = "> ";
+            yield return enableDelayInstruction;
+        }
 
         string[] words = message.Split(' ');
         foreach (string word in words)
@@ -97,6 +130,16 @@ public class LaikaHUD : MonoBehaviour
             yield return deleteDelayInstruction;
         }
 
+        yield return disableDelayInstruction;
+
+        DisableHUD();
         isBusy = false;
+    }
+
+    private void EnableHUDGroup(bool enabled)
+    {
+        hudGroup.alpha = enabled ? 1 : 0;
+        hudGroup.blocksRaycasts = enabled;
+        hudGroup.interactable = enabled;
     }
 }
